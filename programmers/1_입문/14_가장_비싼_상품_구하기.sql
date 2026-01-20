5cd7a45cb43b8c1a3b661878d0318130705747ad
SOLAMU <joeunsol030@gmail.com>
2026-01-20 16:59:58 +0900

    [프로그래머스] 입문 - 가장 비싼 상품 구하기 (#14)

---
+++ programmers/1_입문/14_가장_비싼_상품_구하기.sql
@@ -0,0 +1,10 @@
+-- 가장 비싼 상품 구하기
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/131697
+-- 작성자: 조은솔
+-- 작성일: 2026. 01. 20. 16:59:58
+
+-- 코드를 입력하세요
+SELECT 
+    MAX (price) AS max_price
+FROM product
