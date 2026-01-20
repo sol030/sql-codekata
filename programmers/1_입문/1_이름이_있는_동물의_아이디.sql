10f8d71fb01104819b279938f89e78b7c61a7391
SOLAMU <joeunsol030@gmail.com>
2026-01-20 16:58:48 +0900

    [프로그래머스] 입문 - 이름이 있는 동물의 아이디 (#1)

---
+++ programmers/1_입문/1_이름이_있는_동물의_아이디.sql
@@ -0,0 +1,11 @@
+-- 이름이 있는 동물의 아이디
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59407
+-- 작성자: 조은솔
+-- 작성일: 2026. 01. 20. 16:58:48
+
+-- 코드를 입력하세요
+SELECT animal_id
+FROM animal_ins
+where name IS NOT NULL
+ORDER BY animal_id ASC;
