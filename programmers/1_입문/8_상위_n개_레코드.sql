5c920732175955f140419736356bb442892f551f
SOLAMU <joeunsol030@gmail.com>
2026-01-20 16:58:55 +0900

    [프로그래머스] 입문 - 상위 n개 레코드 (#8)

---
+++ programmers/1_입문/8_상위_n개_레코드.sql
@@ -0,0 +1,11 @@
+-- 상위 n개 레코드
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59405
+-- 작성자: 조은솔
+-- 작성일: 2026. 01. 20. 16:58:55
+
+-- 코드를 입력하세요
+SELECT name
+FROM animal_ins
+ORDER BY datetime
+LIMIT 1;
