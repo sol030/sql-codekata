38470f270315a591cff514b25ce3d74342812d42
SOLAMU <joeunsol030@gmail.com>
2026-01-20 16:57:14 +0900

    [프로그래머스] 입문 - 역순 정렬하기 (#2)

---
+++ programmers/1_입문/2_역순_정렬하기.sql
@@ -0,0 +1,12 @@
+-- 역순 정렬하기
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59035
+-- 작성자: 조은솔
+-- 작성일: 2026. 01. 20. 16:57:14
+
+-- 코드를 입력하세요
+SELECT
+    name,
+    datetime
+FROM animal_ins
+ORDER BY animal_id DESC;
