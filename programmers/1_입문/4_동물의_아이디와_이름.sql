3d0a3435dee56e8b298b9b50b74b67af3d8d486f
SOLAMU <joeunsol030@gmail.com>
2026-01-20 16:57:55 +0900

    [프로그래머스] 입문 - 동물의 아이디와 이름 (#4)

---
+++ programmers/1_입문/4_동물의_아이디와_이름.sql
@@ -0,0 +1,12 @@
+-- 동물의 아이디와 이름
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59403
+-- 작성자: 조은솔
+-- 작성일: 2026. 01. 20. 16:57:55
+
+-- 코드를 입력하세요
+SELECT 
+    animal_id,
+    name
+FROM animal_ins
+ORDER BY animal_id
